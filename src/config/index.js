1abb77cfa35442a1de3fba3f16201e590ef9b8c8
Arielle Janine Sisles <ajsisles@yondu.com>
2026-06-15 09:01:22 +0800

    changes on baseApiUrl for production and local

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 const environmentUrls = new Map();
 
-environmentUrls.set('localhost', 'http://localhost:8080');
-environmentUrls.set('production', 'https://bookstore-production-ccd4.up.railway.app');
-
-export const baseApiUrl = environmentUrls.get(window.location.hostname);
+export const baseApiUrl = 
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '127.0.0.1'
+  ? 'http://localhost:8080'                                // If TRUE (on your laptop)
+  : 'https://bookstore-production-ccd4.up.railway.app';   // If FALSE (live on Railway)
