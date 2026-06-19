789e1eeabbe5723d405219efe034d5e22fbce3ca
Arielle Janine Sisles <ajsisles@yondu.com>
2026-06-19 15:04:45 +0800

    Adding of interceptor to use bearer token on request

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { App } from './component/App';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import reducers from './module';
+import axios from 'axios';
 
 // Modern, robust store setup with built-in Thunk and DevTools configuration
 const store = configureStore({
@@ -11,6 +12,18 @@ const store = configureStore({
     // Redux Toolkit automatically sets up Redux Thunk middleware 
     // and Redux DevTools extension links natively!
 });
+
+axios.interceptors.request.use(
+    (config) => {
+        const token = window.localStorage.getItem('bookstore-token');
+        config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+
+)
 
 const root = createRoot(document.getElementById('root'));
 root.render(
