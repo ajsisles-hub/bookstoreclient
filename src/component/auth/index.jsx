55d322d47323aa675bad9ed25104dbb9c62765e9
Arielle Janine Sisles <ajsisles@yondu.com>
2026-06-22 17:15:33 +0800

    chores: remove unused import

--- src/component/auth/index.jsx
+++ src/component/auth/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { getUserToken } from '../../module/user/userSelector';
 
 
